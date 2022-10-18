db8630b09062053b664a71ca4592b717b22fa615
Antoine Sterna <antoine.Sterna@teamwork.net>
2022-10-18 20:52:13 +0200

    Terraform networking

---
+++ terraform/production-env/output.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+
+  value = module.Networking.vpc_id
+}
+
+output "public_subnets_id" {
+
+  value = module.Networking.public_subnets_id
+}
+
+output "private_subnets_id" {
+
+  value = module.Networking.private_subnets_id
+
+}
+
+output "public_route_table" {
+
+  value = module.Networking.public_route_table
+
+}

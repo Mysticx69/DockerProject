d74aba2ac64d287b052bac6b794d93b598282d1b
Antoine Sterna <sternaantoine@gmail.com>
2022-10-22 17:20:44 +0200

    Added python

--- python/scripts/parse.py
+++ python/scripts/parse.py
@@ -6,7 +6,9 @@ Parse data
 
 def parse_data(data_json):
     """Parsing data for dockerproject"""
-    print("Nos item : \n")
+
+    json_payload = []
+    print("Nos items : \n")
     for item in data_json.get('values'):
 
         addr = (item.get('address'))
@@ -17,6 +19,7 @@ def parse_data(data_json):
         new_json = (f' {{ \n Addresse : {addr} \n  \
                 Commune : {commune} \n \
                 capacite : {capacite_totale} \n \
-                velo dispo {velo_dispo}  \n }},  ')
+                velo dispo {velo_dispo} \n }}, \n  ')
 
         print(new_json)
+        json_payload.append(data_json)

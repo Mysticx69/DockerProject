179ab6c7c9ef83bf8b4d3cb475be1733fd5c4d26
Antoine Sterna <sternaantoine@gmail.com>
2022-10-22 12:16:29 +0200

    Added file provisioner for Ansible auto

--- terraform/production-env/main.tf
+++ terraform/production-env/main.tf
@@ -114,7 +114,8 @@ resource "aws_instance" "Dev_Tools" {
     inline = [
 
       "sudo mv /tmp/hosts /etc/hosts",
-      "sudo chmod 600 /home/ubuntu/.ssh/id_rsa"
+      "sudo chmod 600 /home/ubuntu/.ssh/id_rsa",
+      "sudo chmod 600 /home/ubuntu/.ssh/labsuser.pem"
     ]
 
 

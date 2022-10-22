1da5d5b8ae5f386528893ad1ef75e95ff38e3899
Antoine Sterna <sternaantoine@gmail.com>
2022-10-22 12:40:25 +0200

    Added file provisioner for Ansible auto

--- terraform/production-env/scripts/user-data_Ansible.sh
+++ terraform/production-env/scripts/user-data_Ansible.sh
@@ -15,6 +15,5 @@ mkdir /home/ubuntu/dev
 cd /home/ubuntu/dev
 git clone git@github.com:Mysticx69/DockerProject.git
 chown -R ubuntu:ubuntu /home/ubuntu/dev
-cd DockerProject
-cd ansible
-ansible-playbook -i inventories/production/hosts playbooks/deploy-swarm.yaml
+cd /home/ubuntu/dev/DockerProject/ansible
+sudo -u ubuntu -c 'ansible-playbook -i inventories/production/hosts playbooks/deploy-swarm.yaml'

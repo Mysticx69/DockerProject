7a35739ceaa143afd2d613f5a5c2e2c3fe024b24
Antoine Sterna <sternaantoine@gmail.com>
2022-10-24 00:06:50 +0200

    Added docker and pyhon structure

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -1,40 +1,20 @@
-"""Main module =>
-1. Fetch GrandLyon API
-2. Parse DATA for influxDB format and send it
-@author: asterna, tblanchefort"""
-
-#Imports
-from influxdb_client import InfluxDBClient
+"""
+Parse data
+@author: tblanchefort
+"""
+#####Import#####
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-from fetch import fetch_api
-from parse import parse_data
 
-TOKEN = "5up3r-S3cr3t-auth-t0k3n"
-ORG = "influxdata-org"
-BUCKET = "default"
-URL = "http://localhost:8086"
-DATA = "https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json?maxfeatures=10&start=1"
+#####Initialize the Client###########
 
+# You can generate an API token from the "API Tokens Tab" in the UI
+token = "5up3r-S3cr3t-auth-t0k3n"
+org = "influxdata-org"
+bucket = "default"
 
-def main() -> None:
-    """Main function"""
-    # Fetch API and get JSON response
-    ## Parse Data ans send it to influxDB
-    data_fetched = fetch_api(DATA)
-    print(parse_data(data_fetched))
-
-    with InfluxDBClient(url=URL, token=TOKEN, org=ORG) as client:
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-
-        data = parse_data(data_fetched)
-        write_api.write(BUCKET, ORG, data)
-
-        query = 'from(bucket: "default") |> range(start: -1h)'
-        tables = client.query_api().query(query, org=ORG)
-        for table in tables:
-            for record in table.records:
-                print(record)
-
-
-if __name__ == "__main__":
-    main()
+with InfluxDBClient(url="http://127.0.0.1:8086", token=token, org=org) as client:
+    data = ""
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(bucket, org, data)
